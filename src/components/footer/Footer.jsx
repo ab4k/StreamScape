3c06736c7e5c21cdd1cece7beb7c7b960ddea995
Abhay Kumar <69470032+ab4k@users.noreply.github.com>
2024-01-18 12:37:39 +0530

    Add files via upload

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          We hope you enjoyed browsing our website and found the perfect movie
+          to watch. Our website is dedicated to providing you with the best
+          movie recommendations based on your preferences. Follow us on social
+          media to stay up-to-date with the latest movie news and trends.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
